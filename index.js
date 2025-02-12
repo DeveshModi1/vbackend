babaa042f351b91192b3d25aa4b7bcbeaecd7185
DeveshModi1 <modidevesh15@gmail.com>
2025-02-12 15:58:54 +0530

    Updated backend with latest changes

--- index.js
+++ index.js
@@ -13,14 +13,14 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000", // Allow only frontend domain
+    origin: "https://vastrafusion.com/", // Allow only frontend domain
     credentials: true, // Allow credentials (cookies, auth headers, etc.)
   })
 );
 
 // ✅ Manually add headers for safety
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "https://vastrafusion.com/");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
